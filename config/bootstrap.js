8a6b33966984d361ba6d167bdd9acc2c552e621a
Anton Liulichev <anton.liulichev@gmail.com>
2015-07-27 23:42:39 +0200

    Worker added to fetc fb groups

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,10 +8,29 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+ var http = require('http');
+var groups = ['245976828868737', '857687074272396', '1413120325569205', '1585480988396293']
+var startFetchingGroup = function(groupId){
+	setTimeout(function(){ 
+		DataService.fetchData(groupId, function(data, err){
+			if(err){
+				sails.log(err)
+				return;
+			}
+			sails.log(data.length + ' new items for group ' + groupId)
+			startFetchingGroup(groupId)
+		})
+	 }, 1000);
+}
+
+
+
+
 
 module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  groups.forEach(function(group){startFetchingGroup(group)})
   cb();
 };

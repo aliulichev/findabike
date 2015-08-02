788b0ab47150d8f141d13d3caf2b04a4dd1c5494
Anton Liulichev <anton.liulichev@gmail.com>
2015-08-02 23:42:02 +0200

    Subsribe updates the price now

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,23 +5,44 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+
 module.exports = {
 
 	subscribe:function(req,res){
 		var email = req.param('email');
 		var price = req.param('price')
-		console.log(price)
 		var keywords = req.param('keywords')
-		User.create({fbId:"1", name:"John Smith", email:email, 
-			priceFrom:0, priceTo:price, keywords:keywords}).exec(function(err, user){
-				if(err){
-					res.badRequest(err)
-					return;
-				}
-				sails.log(user)
-				res.send("OK")	
+		User.findOne({ email: email}).exec(function(err, user){
+			 if(err){sails.log(err); res.badRequest(err); return;}
+			 
 
-		})
+			 if(user){
+			 	user.priceTo = price
+			 	user.keywords = keywords
+			 	user.save(function(err){			 	 
+			 		if(err){sails.log(err); res.badRequest(err); return;}
+			 	 	sails.log('Price updated to ' + price)
+			 		res.send("OK");	return;
+
+			 	})
+			 } else {
+			 	User.create({fbId:"1", name:"John Smith", email:email, 
+					priceFrom:0, priceTo:price, keywords:keywords}).exec(function(err, user){
+					if(err){
+						sails.log(err)
+						res.badRequest(err)
+						return;
+					}
+					sails.log(user)
+					res.send("OK")	
+
+				})
+
+			 }	
+			
+
+
+		})	
 		
 	}
 	

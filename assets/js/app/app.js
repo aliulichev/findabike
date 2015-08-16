e270ea0574871d03d3c284023472856e3234eadb
tmwd <tmwd@MacBook-Pro-tmwd.local>
2015-08-16 23:22:17 +0200

    front-end ready

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,1 +1,28 @@
 angular.module('app', ['ngTagsInput']);
+angular.module('app').controller('PageCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.form = {
+        id: null,
+        email: null,
+        price: 50,
+        keywords: []
+    };
+
+    $scope.hide_form = false;
+
+    $scope.submitForm = function () {
+
+        $http.post('/subscribe', {
+            id: $scope.form.id,
+            email: $scope.form.email,
+            price: $scope.form.price,
+            keywords: $scope.form.keywords.map(function (item) {
+                return item.text;
+            })
+        }).
+            then(function (res) {
+                $scope.hide_form = true;
+            }, function (response) {
+            });
+    };
+}]);
